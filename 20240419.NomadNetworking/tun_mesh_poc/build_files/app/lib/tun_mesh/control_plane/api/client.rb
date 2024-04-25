1e64c464bacadd46b7f3ce7fc3575efad3377e5d
Tom Noonan II <t.noonanii@f5.com>
2024-04-25 11:50:14 -0400

    [20240419.NomadNetworking] ENable HTTP persistence

--- 20240419.NomadNetworking/tun_mesh_poc/build_files/app/lib/tun_mesh/control_plane/api/client.rb
+++ 20240419.NomadNetworking/tun_mesh_poc/build_files/app/lib/tun_mesh/control_plane/api/client.rb
@@ -1,5 +1,5 @@
-require 'httparty'
 require 'logger'
+require 'persistent_http'
 require_relative '../structs/registration'
 require_relative '../../ipc/packet'
 
@@ -22,55 +22,57 @@ module TunMesh
          
           @manager = manager
           @logger = Logger.new(STDERR, progname: "#{self.class}(#{remote_url})")
-          @remote_url = remote_url
+
+          @persistent_http = PersistentHTTP.new(
+            :name         => "#{self.class}(#{remote_url})",
+            :logger       => @logger,
+            :pool_size    => 3, # Only expected to be accessed by the Registrations::RemoteNode worker and the Registrations worker
+            :pool_timeout => 5,
+            :warn_timeout => 0.25,
+            :force_retry  => true,
+            :url          => remote_url
+          )
         end
 
         def register(payload:)
           raise(ArgumentError, "Payload must be a TunMesh::ControlPlane::Structs::Registration, got #{payload.class}") unless payload.is_a? TunMesh::ControlPlane::Structs::Registration
-          return _post(path: 'tunmesh/control/v0/registrations/register',
+          return _post(path: '/tunmesh/control/v0/registrations/register',
                        payload: payload)
         end
 
         def transmit_packet(packet:)
           raise(ArgumentError, "Packet must be a TunMesh::IPC::Packet, got #{packet.class}") unless packet.is_a? TunMesh::IPC::Packet
-          return _post(path: 'tunmesh/control/v0/packet/rx',
+          return _post(path: '/tunmesh/control/v0/packet/rx',
                        payload: packet)
         end
 
         private
 
         def _post(path:, payload:)
-          # TODO: URL builder lib
-          url = "#{@remote_url}/#{path}"
-          @logger.debug { "Performing HTTP POST to #{url}" }
-        
-          # TODO: Connection reuse
-          resp = HTTParty.post(url,
-                               body: payload.to_json,
-                               headers: {
-                                 'Authorization' => "Bearer: #{@manager.api_auth.new_token}",
-                                 'Content-Type' => 'application/json'
-                               },
-                               # As this is using advertised, dynamic URLs the CN won't match.
-                               # HTTParty doesn't give us enough granularity over the SSL API to verify the CA but not the CN
-                               # Impersonation attacks are intended to be caught by the request JWT auth
-                               verify: false 
-                            )
-          return nil if resp.code == 204
-          raise(RequestException.new("HTTP POST to #{url} returned #{resp.code}", resp.code)) if resp.code < 200 || resp.code >= 300
+          @logger.debug { "Performing HTTP POST to #{path}" }
+          request = Net::HTTP::Post.new(path)
+          # NOTE: No SSL Verification
+          # As this is expected to be using using advertised, dynamic URLs based on IP:Port combos the CN won't match.
+          # Impersonation attacks are intended to be caught by the request JWT auth
+          request.body = payload.to_json
+          request.content_type = 'application/json'
+          request['Authorization'] = @manager.api_auth.new_http_authorization_header_value
 
-          raise(RequestException.new("HTTP POST to #{url} returned unexpected content/code #{resp.headers['content-type']} / #{resp.code}", resp.code)) unless resp.code == 200
-          raise(RequestException.new("HTTP POST to #{url} returned content type #{resp.headers['content-type']}", resp.code)) if resp.headers['content-type'] != 'application/json'
+          resp = @persistent_http.request(request)
+          return nil if resp.code == '204'
+          raise(RequestException.new("HTTP POST to #{path} returned #{resp.code}", resp.code)) if resp.code.to_i < 200 || resp.code.to_i >= 300
+
+          raise(RequestException.new("HTTP POST to #{path} returned unexpected content/code #{resp.content_type} / #{resp.code}", resp.code)) unless resp.code == '200'
+          raise(RequestException.new("HTTP POST to #{path} returned content type #{resp.content_type}", resp.code)) if resp.content_type != 'application/json'
 
           begin
-            token_claims = @manager.api_auth.verify_http_authorization_header_value(resp.headers['authorization'])
+            token_claims = @manager.api_auth.verify_http_authorization_header_value(resp['authorization'])
             # TODO: Improve this
             raise("Response authentication issuer mismatch.") if token_claims['iss'] == @manager.id
           rescue StandardError => exc
-            raise(RequestException.new("HTTP POST to #{url} failed response authorization verification: #{exc.class}: #{exc}", resp.code))
+            raise(RequestException.new("HTTP POST to #{path} failed response authorization verification: #{exc.class}: #{exc}", resp.code))
           end
-          
-          
+
           return resp.body
         end
       end
